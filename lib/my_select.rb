e182dd345cb415b6145865f3669062754b6686b6
christine gomez <christine.gomez1226@gmail.com>
2018-09-06 20:21:46 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,10 +1,9 @@
-nums = [1,2,3,4,5]
-def my_select(collection)
- new_arr=[]
- i=0
- while i <collection.length
-   new_arr.push yield(collection[i])
-   i+=1
+def my_select(array)
+  new_arr=[]
+  i = 0
+  while i<array.length
+    new_arr.push yield(array[i])
+    i+=1
  end
  new_arr
 end
